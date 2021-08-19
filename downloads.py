e2c2fb2bf79720587a62ea6ac4de6778a10219c8
Alastair Montgomery <alastair.montgomery@engineering.digital.dwp.gov.uk>
2021-08-19 16:31:32 +0100

    Debuging

--- downloads.py
+++ downloads.py
@@ -25,4 +25,4 @@ for filename in os.listdir(directory):
             sub_directory = file_folder_mapping.get(ext[1].lower(), "misc")
             if not os.path.exists(sub_directory):
                 os.makedirs(sub_directory)
-            os.rename(f, os.path.join(sub_directory, f))
+            os.rename(f, os.path.join(sub_directory, os.path.basename(f)))
