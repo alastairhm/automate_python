5fc2eb491afa946b0ede50897332f1ca5e6dbe9e
Alastair Montgomery <alastair.montgomery@engineering.digital.dwp.gov.uk>
2022-04-07 13:44:47 +0100

    Simple password gen

---
+++ password.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import secrets
+import string
+import argparse
+
+parser = argparse.ArgumentParser(description='Password Generator')
+parser.add_argument("-l","--length",type=int,help="Length of password",
+                        default=35)
+args = parser.parse_args()
+
+print(args)
+
+alphabet = string.ascii_letters + string.digits + string.punctuation
+password = ''.join(secrets.choice(alphabet) for i in range(args.length))
+print(password)
+
