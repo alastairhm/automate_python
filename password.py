7509c1aaf751c464e07782d3610ddd1d0cb4f7b2
Alastair Montgomery <alastair.montgomery@engineering.digital.dwp.gov.uk>
2022-04-07 13:57:17 +0100

    Simple password gen

--- password.py
+++ password.py
@@ -8,8 +8,6 @@ parser.add_argument("-l","--length",type=int,help="Length of password",
                         default=35)
 args = parser.parse_args()
 
-print(args)
-
 alphabet = string.ascii_letters + string.digits + string.punctuation
 password = ''.join(secrets.choice(alphabet) for i in range(args.length))
 print(password)
