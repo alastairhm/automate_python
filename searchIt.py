66f0d1ed06f3b71bc9dfa488cb8fef0615517a15
Alastair Montgomery <alastair.montgomery@engineering.digital.dwp.gov.uk>
2022-04-21 10:45:11 +0100

    feat:add settings file for search it

--- searchIt.py
+++ searchIt.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
+'''Quick searching from command line'''
 
 import webbrowser
 import sys
+import os
 import pyperclip
+import toml
+
+script_path = os.path.dirname(os.path.abspath(__file__))
+settings = toml.load(os.path.join(script_path, "searchIt.toml"))
+engine = settings["engine"]
+search_url = settings[engine]
 
 if len(sys.argv) > 1:
-    address = " ".join(sys.argv[1:])
+    search_term = " ".join(sys.argv[1:])
 else:
-    address = pyperclip.paste()
+    search_term = pyperclip.paste()
 
-webbrowser.open("https://duckduckgo.com/?q=" + address)
+webbrowser.open(search_url + search_term)
