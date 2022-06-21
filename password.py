c7bf6c9c4030dbc9e45c088384dcd80941c4a3c6
Alastair Montgomery <alastair.montgomery@engineering.digital.dwp.gov.uk>
2022-06-21 16:31:45 +0100

    feat: update password script, to return a number of passwords

--- password.py
+++ password.py
@@ -3,12 +3,19 @@ import secrets
 import string
 import argparse
 
+def get_password(length):
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(secrets.choice(alphabet) for i in range(length))
+    return password
+
+
 parser = argparse.ArgumentParser(description='Password Generator')
 parser.add_argument("-l","--length",type=int,help="Length of password",
                         default=35)
+parser.add_argument("-n","--number",type=int,help="Number of passwords",
+                        default=10)
 args = parser.parse_args()
 
-alphabet = string.ascii_letters + string.digits + string.punctuation
-password = ''.join(secrets.choice(alphabet) for i in range(args.length))
-print(password)
+for i in range(args.number):
+    print(get_password(args.length))
 
