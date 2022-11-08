44a5f076eee3ad35b0659468d187ded4cbd70fe5
Alastair Montgomery <alastair.montgomery@bjss.com>
2022-11-08 15:17:53 +0000

    Option to copy password to clipboard

--- password.py
+++ password.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
+import argparse
+import pyperclip
 import secrets
 import string
-import argparse
-
 
 def get_password(length):
     alphabet = string.ascii_letters + string.digits + string.punctuation
@@ -12,8 +12,12 @@ def get_password(length):
 
 parser = argparse.ArgumentParser(description="Password Generator")
 parser.add_argument("-l", "--length", type=int, help="Length of password", default=35)
-parser.add_argument("-n", "--number", type=int, help="Number of passwords", default=10)
+parser.add_argument("-n", "--number", type=int, help="Number of passwords", default=1)
+parser.add_argument("-c", "--clipboard", action='store_true')
 args = parser.parse_args()
 
 for i in range(args.number):
-    print(get_password(args.length))
+    pword = get_password(args.length)
+    print(pword)
+    if args.clipboard :
+        pyperclip.copy(pword)      
