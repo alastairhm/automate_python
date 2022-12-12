8a677f29b464a6b6ccf969d16ea191868200e7fb
Alastair Montgomery <alastair.montgomery1@nhs.net>
2022-12-12 09:47:34 +0000

    Add flag for special characters

--- password.py
+++ password.py
@@ -4,8 +4,12 @@ import pyperclip
 import secrets
 import string
 
-def get_password(length):
-    alphabet = string.ascii_letters + string.digits + string.punctuation
+
+def get_password(length, special):
+    if special:
+        alphabet = string.ascii_letters + string.digits + string.punctuation
+    else:
+        alphabet = string.ascii_letters + string.digits
     password = "".join(secrets.choice(alphabet) for i in range(length))
     return password
 
@@ -13,11 +17,12 @@ def get_password(length):
 parser = argparse.ArgumentParser(description="Password Generator")
 parser.add_argument("-l", "--length", type=int, help="Length of password", default=35)
 parser.add_argument("-n", "--number", type=int, help="Number of passwords", default=1)
-parser.add_argument("-c", "--clipboard", action='store_true')
+parser.add_argument("-c", "--clipboard", action="store_true")
+parser.add_argument("-s", "--special", action="store_true")
 args = parser.parse_args()
 
 for i in range(args.number):
-    pword = get_password(args.length)
+    pword = get_password(args.length, args.special)
     print(pword)
-    if args.clipboard :
-        pyperclip.copy(pword)      
+    if args.clipboard:
+        pyperclip.copy(pword)
